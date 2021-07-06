516760065f480b04ad1ce769b72762b7f03738f5
devjomaa <dakrnaik2000@gmail.com>
2021-07-07 00:03:33 +0300

    done home page

--- js/app.js
+++ js/app.js
@@ -12,3 +12,16 @@ pop_up_to_top.addEventListener('click',()=>{
     })
 })
 
+
+// update time and date
+
+let hour =new Date().getHours()
+let minut =new Date().getMinutes()
+let format = hour >=12 ? 'PM':'AM'
+
+let month =new Date().getMonth() +1
+let today =new Date().getDate()
+let year =new Date().getFullYear()
+
+document.querySelector('.time_is').innerHTML = `${hour}:${minut} ${format}`
+document.querySelector('.date_today').innerHTML = `${today}/${month}/${year}`
